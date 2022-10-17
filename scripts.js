29cade6fabd5d2353db787876eb93e911c5f6ee7
sarahhoppe19 <sarah.hoppe@wsu.edu>
2022-10-17 03:21:45 +0000

    added two recipes from peers

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-//JavaScript for TP6
+//JavaScript for TP7
 
 function loadFileInto(fromFile, whereTo) {
 
@@ -24,6 +24,39 @@ function loadFileInto(fromFile, whereTo) {
 
 }
 
+//new recipe object
+function Recipe(recipeName, contributorName, imageURL, ingredientsURL, equipmentURL, directionsURL) {
+  this.recipeName = recipeName;
+  this.contributor = contributorName;
+  this.image = imageURL;
+  this.ingredients = ingredientsURL;
+  this.equipment = equipmentURL;
+  this.directions = directionsURL;
+  
+  this.displayRecipe = function() {
+    document.querySelector("#recipeBanner h1").innerHTML = this.recipeName;
+    document.querySelector("#contributor").innerHTML = this.contributor;
+    document.querySelector("#recipeBanner").style.backgroundImage = "url(" + this.image + ")";
+    loadFileInto(this.ingredients, "#ingredients ul");
+    loadFileInto(this.equipment, "#equipment ul");
+    loadFileInto(this.directions, "#directions ol");
+  }
+}
+
+MushroomSalsaChili = new Recipe("Mushroom Salsa Chili", "Sarah Hoppe", "chiliPhoto2.jpg", "ingredients.html", "equipment.html", "directions.html");
+BasicCrepes = new Recipe("Basic Crepes", "Zakiya Pruitt", "crepesPhoto.jpg", "ingredientsCrepes.html", "equipmentCrepes.html", "directionsCrepes.html");
+Pancakes = new Recipe("Old Fashioned Pancakes", "Maddy Szczypka", "pancakesPhoto.jpg", "ingredientsPancakes.html", "equipmentPancakes.html", "directionsPancakes.html");
+
 window.onload = function() {
+ document.querySelector("#firstRecipe").onclick = function() {
+   MushroomSalsaChili.displayRecipe();
+ }
  
+ document.querySelector("#secondRecipe").onclick = function() {
+   BasicCrepes.displayRecipe();
+ }
+ 
+ document.querySelector("#thirdRecipe").onclick = function() {
+   Pancakes.displayRecipe();
+ }
 } //end of window.onload
